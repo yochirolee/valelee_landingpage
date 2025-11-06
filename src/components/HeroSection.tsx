768272124b427fb4f7869e607d233b32ed7f5b5a
Leidiana Torres Roca <yleecruz@gmail.com>
2025-11-06 14:30:26 -0500

    feat: initial commit (Valelee website)

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -190,7 +190,7 @@ export default function HeroSection({ dict }: { dict: Dict }) {
       <main className="relative isolate overflow-hidden pt-0">
         <section className="bg-[#F6F3EE]">
           {/* Aire para el header fijo */}
-          <div className="pt-15 sm:pt-15 lg:pt-15">
+          <div className="pt-1 sm:pt-1 lg:pt-1">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 sm:py-16 lg:py-20">
 
               {/* CARD COMBINADO */}
